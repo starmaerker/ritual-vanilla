5f13367f1be496a5145d3e2d8b80bc96e841b43c
starmaerker <starmaerker@googlemail.com>
2018-10-18 13:42:13 +0200

    refined firebase

--- js/index.js
+++ js/index.js
@@ -1,13 +1,49 @@
-function collectData() {    
+var database = firebase.database();
+var db;
+
+function collectData() {
     let data = {};
     let name = document.getElementsByName("name");
     let email = document.getElementsByName("email");
     let age = document.getElementsByName("age");
 
-    data[name[0].name] = name[0].innerHTML;
-    data[email[0].name] = email[0].innerHTML;
-    data[age[0].name] = age[0].innerHTML;
 
-    console.log(data);
-  
+    data[name[0].name] = name[0].value;
+    data[email[0].name] = email[0].value;
+    data[age[0].name] = age[0].value;
+
+    
+    /*
+    alert(db.email + " " + email[0].value);
+
+    if (db.email ===  email[0].value) {
+        alert("This email already submitted the survey.");
+    } else {
+        
+        database.ref().child('user-data').update(data);
+    }
+    */
+
+
 }
+
+function updateFirebase(name, value) {
+    //database.ref().child('user-data').push().key
+
+    database.ref().child('user-data').update({
+        [name]: value,
+    });
+}
+
+var snap = function retrieveFromFirebase() {
+    return database.ref().child('user-data').once('value').then(function (snapshot) {
+        db = snapshot.val();
+        testFn(db);
+    });
+}
+
+function testFn(input) {
+    alert(input.email);
+}
+
+// snap();
