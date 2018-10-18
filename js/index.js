3e5187b37e5ece74148bbeb717130e5af219182a
starmaerker <starmaerker@googlemail.com>
2018-10-18 10:41:18 +0200

    added firebase

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-let data = {};
+function collectData() {    
+    let data = {};
+    let name = document.getElementsByName("name");
+    let email = document.getElementsByName("email");
+    let age = document.getElementsByName("age");
 
-function collectData() {
-    
-    data["name"] = "success";    
-   
+    data[name[0].name] = name[0].innerHTML;
+    data[email[0].name] = email[0].innerHTML;
+    data[age[0].name] = age[0].innerHTML;
+
+    console.log(data);
+  
 }
